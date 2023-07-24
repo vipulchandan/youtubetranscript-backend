60e26a6fe3b65c95c5456f3fddc9d335c4994a1e
vipulchandan <plenteousvipul90009@gmail.com>
2023-07-24 18:42:25 +0530

    fixed youtube url link

--- controllers/TranscriptController.js
+++ controllers/TranscriptController.js
@@ -21,20 +21,28 @@ async function fetchVideoInfo(videoUrl) {
 
 // Fetch video transcript using the youtube-captions-scraper package
 async function fetchVideoTranscript(videoId) {
-  const captions = await getSubtitles({
-    videoID: videoId,
-    lang: 'en', // Assuming English captions are required
-  });
+  try {
+    const captions = await getSubtitles({
+      videoID: videoId,
+      lang: 'en', // English captions
+    });
 
-  if (!captions || captions.length === 0) {
-    return { transcript: "No transcript available" }; 
+    if (!captions || captions.length === 0) {
+      // Return null or an indicator for missing subtitles
+      return { transcript: null };
+    }
+
+    const transcriptText = captions.reduce((acc, caption) => {
+      return acc + caption.text + ' \n';
+    }, '');
+
+    return { transcript: transcriptText };
+  } catch (error) {
+    console.error(error);
+    // In case of an error, return an object with a transcript property
+    // and set it to null or an appropriate error message
+    return { transcript: null };
   }
-
-  const transcriptText = captions.reduce((acc, caption) => {
-    return acc + caption.text + ' \n';
-  }, '');
-
-  return { transcript: transcriptText };
 }
 
 
@@ -43,9 +51,17 @@ const createVideoTranscript = async (req, res) => {
     const { videoUrl } = req.body;
 
     if (!videoUrl) {
-        return res.status(400).json({ 
-            error: 'videoUrl is missing in the request body.'
-        });
+      return res.status(400).json({
+        error: 'videoUrl is missing in the request body.',
+      });
+    }
+
+    // Check if the youtube URL is valid
+    const regex = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=([^&]+).*/;
+    if (!regex.test(videoUrl)) {
+      return res.status(400).json({
+        error: 'Please enter a valid YouTube video URL.',
+      });
     }
 
     const videoId = extractVideoId(videoUrl);
@@ -65,13 +81,18 @@ const createVideoTranscript = async (req, res) => {
         videoId,
         title: videoInfo.title,
         videoUrl,
-        transcript: transcriptData.transcript,
+        transcript: transcriptData.transcript, // It can be null or actual transcript based on the video
       });
 
       await video.save();
 
+      // Check if the transcript is null and set an appropriate message
+      const transcriptMessage = transcriptData.transcript === null
+        ? 'Transcript not available for this video.'
+        : 'Video saved successfully.';
+
       res.status(201).json({
-        message: 'Video saved successfully.',
+        message: transcriptMessage,
         videoUrl,
         transcript: transcriptData.transcript,
       });
@@ -79,7 +100,7 @@ const createVideoTranscript = async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(500).json({
-        message: error.message,
+      message: error.message,
     });
   }
 };
